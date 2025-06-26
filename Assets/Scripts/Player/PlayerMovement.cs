9338835f0ef17c47d1a30c3723a937f05f873697
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2025-06-27 04:44:02 +0900

    플레이어 작업
    플레이어 이동 작업
    New Input System을 통한 이동 키 매핑 작업
    인터페이스 매칭
    업데이트 매니저 배치 및 활용
    crosshair 적용

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveDirection;
+    Rigidbody2D rigidBody2D;
+
+    float moveSpeed = 5f;
+    float jumpForce = 10f;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        if (input != null)
+        {
+            moveDirection = new Vector2(input.x, input.y);
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rigidBody2D.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody2D.velocity = new Vector2(moveDirection.x * moveSpeed, rigidBody2D.velocity.y);
+    }
+}
