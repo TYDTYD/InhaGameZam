c274a110977078a06f0511a089639e7ca4193e3a
SeungMin Lee <tmdals5587@naver.com>
2025-06-28 20:12:10 +0900

    스테미나 바 투명 연출 추가

--- Assets/Scripts/Player/PlayerStamina.cs
+++ Assets/Scripts/Player/PlayerStamina.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class PlayerStamina : MonoBehaviour
 {
     [SerializeField] StaminaBar[] staminaBar;
-
+    [SerializeField] CanvasGroup staminaGroup;
     float maxStamina = 10f;
     float currentStamina;
     float recovery = 0.01f;
+    float transparentRate = 0.02f;
     
     void Awake()
     {
@@ -17,7 +18,6 @@ public class PlayerStamina : MonoBehaviour
         }        
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (maxStamina > currentStamina)
@@ -28,12 +28,20 @@ public class PlayerStamina : MonoBehaviour
                 stamina.SetStamina(currentStamina);
             }            
         }
+        else
+        {
+            if (staminaGroup.alpha != 0f)
+            {
+                staminaGroup.alpha -= transparentRate;
+            }
+        }
     }
 
     public bool CanDash(float cost) => currentStamina > cost;
     public float CurrentStamina => currentStamina;
     public void UseStamina(float value)
     {
+        staminaGroup.alpha = 1f;
         currentStamina = Mathf.Max(currentStamina - value, 0f);
         foreach (var stamina in staminaBar)
         {
