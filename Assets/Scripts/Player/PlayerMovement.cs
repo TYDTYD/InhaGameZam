5f9cf04b51880a19d1ba5ee33041d1e924be3530
SeungMin Lee <tmdals5587@naver.com>
2025-06-28 14:41:14 +0900

    벽 점프 기능 구현
    벽에 붙어있을 때, 벽을 밀어내고 점프합니다.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,16 +5,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveDirection;
     Rigidbody2D rigidBody2D;
     PlayerCollisionCheck collisionCheck;
+    PlayerWallCollisionCheck wallCollisionCheck;
 
     float moveSpeed = 5f;
     float jumpForce = 15f;
+    float wallPushForce = 8f;
+    float wallJumpCooldown = 0.1f;
+    float wallJumpTimer = 0f;
     bool jumped = false;
     bool doubleJumped = false;
-    bool wallContacted = false;
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         collisionCheck = GetComponentInChildren<PlayerCollisionCheck>();
+        wallCollisionCheck = GetComponent<PlayerWallCollisionCheck>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -26,36 +30,67 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void OnJump(InputAction.CallbackContext context)
+    void OnGroundJump()
     {
-        if (context.performed)
+        if (collisionCheck.groundCheck)
         {
-            if (collisionCheck.groundCheck)
-            {
-                jumped = false;
-                doubleJumped = false;
+            jumped = false;
+            doubleJumped = false;
 
-                if (!jumped)
-                {
-                    rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                    jumped = true;
-                }
-            }
-            else if (!jumped)
+            if (!jumped)
             {
                 rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                 jumped = true;
             }
-            else if (!doubleJumped)
+        }
+        else if (!jumped)
+        {
+            rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumped = true;
+        }
+        else if (!doubleJumped)
+        {
+            rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            doubleJumped = true;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            switch (wallCollisionCheck.wallContacted)
             {
-                rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                doubleJumped = true;
+                case ContactInfo.RIGHTWALL:
+                    {
+                        wallJumpTimer = wallJumpCooldown;
+                        rigidBody2D.AddForce(new Vector2(-wallPushForce, jumpForce), ForceMode2D.Impulse);
+                        break;
+                    }
+                case ContactInfo.LEFTWALL:
+                    {
+                        wallJumpTimer = wallJumpCooldown;
+                        rigidBody2D.AddForce(new Vector2(wallPushForce, jumpForce), ForceMode2D.Impulse);
+                        break;
+                    }
+                case ContactInfo.NONE:
+                    {
+                        OnGroundJump();
+                        break;
+                    }
             }
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rigidBody2D.velocity = new Vector2(moveDirection.x * moveSpeed, rigidBody2D.velocity.y);
+        if (wallJumpTimer <= 0)
+        {
+            rigidBody2D.velocity = new Vector2(moveDirection.x * moveSpeed, rigidBody2D.velocity.y);
+        }
+        else
+        {
+            wallJumpTimer -= Time.fixedDeltaTime;
+        }
     }
 }
