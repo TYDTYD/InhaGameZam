00c8f983a39530fd8fd4efa20251298e27f08f00
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2025-06-28 15:54:56 +0900

    Monster Collider 변경 및 몬스터 이동 패턴 로직 수정

--- Assets/Scripts/Monster/3)Detector/HorizontalRangeDetector.cs
+++ Assets/Scripts/Monster/3)Detector/HorizontalRangeDetector.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class HorizontalRangeDetector : IPlayerDetector
 {
     private float rangeX;
+    private float rangeY;
     private Transform monsterTransform;
-    public HorizontalRangeDetector(float rangeX, Transform monsterTransform)
+    public HorizontalRangeDetector(float rangeX, Transform monsterTransform, float rangeY = 1.0f)
     {
         this.rangeX = rangeX;
+        this.rangeY = rangeY;
         this.monsterTransform = monsterTransform;
     }
 
     public bool IsPlayerDetected(Transform monster, Transform player)
     {
         float dx = player.position.x - monster.position.x;
+        float dy = Mathf.Abs(player.position.y - monster.position.y);
         bool playerInFront =
             (dx > 0 && monsterTransform.localScale.x > 0) || // 오른쪽 바라보고 오른쪽에 있음
             (dx < 0 && monsterTransform.localScale.x < 0);   // 왼쪽 바라보고 왼쪽에 있음
-        return Mathf.Abs(dx) < rangeX && playerInFront;
+        
+        return Mathf.Abs(dx) < rangeX && dy < rangeY && playerInFront;
     }
 }
