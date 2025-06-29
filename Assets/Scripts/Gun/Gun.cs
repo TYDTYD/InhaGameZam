1b5da956393f3821f3d010904c27e5b33bc01309
SeungMin Lee <tmdals5587@naver.com>
2025-06-29 18:23:05 +0900

    플레이어 기본 방향 및 총 발사 적용
    애니메이션 추가 적용 필요

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 public class Gun : MonoBehaviour
 {
     [SerializeField] ObjectPooling objectPool;
     [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] InputActionReference aimAction;
+
     float fireRate = 0.1f;
     float nextTimeToShoot;
     float missileRate = 3f;
     float nextTimeToMissileShoot;
+
     private void FixedUpdate()
     {
+
         if (Input.GetMouseButton(0) && Time.fixedTime > nextTimeToShoot && objectPool != null)
         {
             Shoot();
@@ -26,10 +33,11 @@ public class Gun : MonoBehaviour
 
         if (bullet == null)
             return;
-            
 
+        Vector2 mouseScreenPos = aimAction.action.ReadValue<Vector2>();
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector2(mouseScreenPos.x, mouseScreenPos.y));
         bullet.transform.SetPositionAndRotation(bulletSpawnPoint.transform.position, bulletSpawnPoint.rotation);
-        Vector2 dir = bulletSpawnPoint.transform.position - transform.parent.position;
+        Vector2 dir = mouseWorldPos - (Vector2)bulletSpawnPoint.transform.position;
         bullet.Fire(dir);
 
         bullet.Deactivate();
@@ -44,9 +52,10 @@ public class Gun : MonoBehaviour
         if (missile == null)
             return;
 
-
+        Vector2 mouseScreenPos = aimAction.action.ReadValue<Vector2>();
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector2(mouseScreenPos.x, mouseScreenPos.y));
         missile.transform.SetPositionAndRotation(bulletSpawnPoint.transform.position, bulletSpawnPoint.rotation);
-        Vector2 dir = bulletSpawnPoint.transform.position - transform.parent.position;
+        Vector2 dir = mouseWorldPos - (Vector2)bulletSpawnPoint.transform.position;
         missile.Fire(dir);
 
         missile.Deactivate();
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,7 +19,8 @@ public class PlayerMovement : MonoBehaviour
     PlayerCollisionCheck collisionCheck;
     PlayerWallCollisionCheck wallCollisionCheck;
     PlayerStamina playerStamina;
-    SpriteRenderer spriteRenderer;
+    MeshRenderer[] meshRenderers;
+    [SerializeField] InputActionReference aimAction;
 
     float moveSpeed = 5f;
     float dashSpeed = 20f;
@@ -29,13 +31,14 @@ public class PlayerMovement : MonoBehaviour
     float wallJumpTimer = 0f;
     int jumpCount = 0;
     int maxJumpCount = 2;
+    bool isDash = false;
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
         collisionCheck = GetComponentInChildren<PlayerCollisionCheck>();
         wallCollisionCheck = GetComponent<PlayerWallCollisionCheck>();
         playerStamina = GetComponent<PlayerStamina>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
     }
 
     // New Input System으로 입력을 받아 움직이는 함수
@@ -103,34 +106,49 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            if (playerStamina.CanDash(dashStamina) && moveDirection.x != 0)
+            if (playerStamina.CanDash(dashStamina) && moveDirection.x != 0 && !isDash)
             {
                 SetMoveState(MoveState.DASH);
                 playerStamina.UseStamina(dashStamina);
                 StartCoroutine(Dash());
             }
         }
+        else if (context.canceled)
+        {         
+            SetMoveState(MoveState.NONE);
+        }
     }
 
     // 실제 대시 동작 구현 함수
     IEnumerator Dash()
     {
+        isDash = true;
         float speed = moveSpeed;
         moveSpeed = dashSpeed;
 
         // 몬스터와 플레이어와의 레이어로 충돌 무시
         // 9 플레이어, 13 enemies
         Physics2D.IgnoreLayerCollision(9, 13, true);
-        float alpha = spriteRenderer.color.a;
-        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.6f);
+
+        List<float> alphas = new List<float>();
+        foreach(var mesh in meshRenderers)
+        {
+            alphas.Add(mesh.material.color.a);
+            mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, 0.6f);
+        }
 
         yield return new WaitForSeconds(0.2f);
 
-        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, alpha);
+        int index = 0;
+        foreach (var mesh in meshRenderers)
+        {
+            mesh.material.color = new Color(mesh.material.color.r, mesh.material.color.g, mesh.material.color.b, alphas[index++]);
+        }
         // 몬스터와 플레이어와의 레이어 충돌 다시 켜기
         Physics2D.IgnoreLayerCollision(9, 13, false);
         SetMoveState(MoveState.MOVE);
         moveSpeed = speed;
+        isDash = false;
     }
 
     void SetMoveState(MoveState moveState) => this.moveState = moveState;
@@ -138,6 +156,20 @@ public class PlayerMovement : MonoBehaviour
     // 벽 점프 쿨타임인지 아닌지 확인한 후, 속도 적용하는 함수
     void FixedUpdate()
     {
+        Vector2 mouseScreenPos = aimAction.action.ReadValue<Vector2>();
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);        
+
+        float offsetX = mouseWorldPos.x - transform.position.x;
+
+        Vector3 scale = transform.localScale;
+
+        if (offsetX > 0)
+            scale.x = Mathf.Abs(scale.x); // 오른쪽 → 양수
+        else if (offsetX < 0)
+            scale.x = -Mathf.Abs(scale.x); // 왼쪽 → 음수
+
+        transform.localScale = scale;
+
         if (wallJumpTimer <= 0)
         {           
             rigidBody2D.velocity = new Vector2(moveDirection.x * moveSpeed, rigidBody2D.velocity.y);
